5de465274582ab68c6a0dc5d84432a3503c6c041
Lakshya Saxena <42578328+lakshyasaxena@users.noreply.github.com>
2020-04-16 20:02:26 +0530

    Fast Exponentiation Algorithm (without modulo)
    In this file, the fast exponentiation algorithm is implemented. But the output is not provided by performing modulo operation. So it may not work for extremely large numbers. The datatype used is long long, so it can store 18 digits number.

---
+++ fast_expo.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ll power(ll a,ll b);
+int main()
+{
+    ll a=10,b=18;
+    cout<<power(a,b);
+    return 0;
+}
+ll power(ll a,ll b)
+{
+    if(b==0) return 1;
+    ll half=power(a,b/2);
+    ll ret=half*half;
+    if(b&1==1) ret*=a;
+    return ret;
+}
