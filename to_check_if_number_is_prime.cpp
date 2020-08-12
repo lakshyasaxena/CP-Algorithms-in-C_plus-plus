b0000249c7f9ba6eb77c821d3a7831f3d2648431
Lakshya Saxena <42578328+lakshyasaxena@users.noreply.github.com>
2020-08-12 10:56:51 +0530

    To check if the number is prime or not

--- to_check_if_number_is_prime.cpp
+++ to_check_if_number_is_prime.cpp
@@ -1,1 +1,33 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+bool check_prime(ll num)
+{
+    if(num==1) return false;
+    for(int i=2;i<=sqrt(num);i++)
+    {
+        if(num%i==0) return false;
+    }
+    return true;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cout<<"  Range 1 to 999999999999999999\n\n";
+    while(1)
+    {
+        cout<<"Enter your number -> ";
+        ll num;
+        cin>>num;
+        bool check=check_prime(num);
+        if(check==false)
+        {
+            cout<<"  It is not a prime\n\n";
+        }
+        else
+        {
+            cout<<"  It is a prime number\n\n";
+        }
+    }
+}
