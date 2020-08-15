0571b3e6b05654480983f3c7a2dcac88e4c99437
Lakshya Saxena <42578328+lakshyasaxena@users.noreply.github.com>
2020-08-15 12:56:10 +0530

    BREADTH FIRST SEARCH GRAPH TRAVERSAL
    BREADTH FIRST SEARCH GRAPH TRAVERSAL

--- Breadth_First_Search.cpp
+++ Breadth_First_Search.cpp
@@ -1,12 +1,18 @@
-
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 //  Given Undirected Graph
-//     0--1--2
-//     | /| /
-//     |/ |/
-//     4--3
+//
+//              1        3
+//             /        /
+//            /        /
+//           0        2____
+//            \    _/ |    \
+//             \ _/   |     \
+//              8     5      4
+//               \   /       |
+//                \ /        |
+//                 6---------7
 //
 void adjacency_list_traverse(vector <vector <ll>> &graph)
 {
@@ -47,13 +53,17 @@ void bfs(vector <vector <ll>> &graph,ll cn)
 }
 int main()
 {
-    ll nodes=5,edges=14;
+    ll nodes=9,edges=20;
     vector <vector <ll>> graph(nodes);
-    graph[0].push_back(1);graph[0].push_back(4);
-    graph[1].push_back(0);graph[1].push_back(4);graph[1].push_back(3);graph[1].push_back(2);
-    graph[2].push_back(1);graph[2].push_back(3);
-    graph[3].push_back(4);graph[3].push_back(1);graph[3].push_back(2);
-    graph[4].push_back(0);graph[4].push_back(1);graph[4].push_back(3);
+    graph[0].push_back(1);graph[0].push_back(8);
+    graph[1].push_back(0);
+    graph[2].push_back(3);graph[2].push_back(4);graph[2].push_back(5);graph[2].push_back(8);
+    graph[3].push_back(2);
+    graph[4].push_back(2);graph[4].push_back(7);
+    graph[5].push_back(2);graph[5].push_back(6);
+    graph[6].push_back(5);graph[6].push_back(7);graph[6].push_back(8);
+    graph[7].push_back(4);graph[7].push_back(6);
+    graph[8].push_back(0);graph[8].push_back(2);graph[8].push_back(6);
 
     adjacency_list_traverse(graph);
     ll cn;  // Current Node/starting node
